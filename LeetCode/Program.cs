8af74027f34e6f6b281f9da86ed3ab2bbf1750f9
Ryan Liang <ryanliang88@outlook.com>
2019-08-02 23:01:09 -0700

    Cleaned Main output

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,7 +7,13 @@ namespace LeetCode
     {
         private static void Main()
         {
-            new TwoSum().Solution(new int[] { 2, 7, 11, 15 }, 9).ToList().ForEach(Console.WriteLine);
+            var twoSumAnswer = new TwoSum().Solution(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine(Formatter.Format(twoSumAnswer));
         }
+    }
+
+    public static class Formatter
+    {
+        public static string Format(int[] nums) => $"[{string.Join(", ", nums)}]";
     }
 }
