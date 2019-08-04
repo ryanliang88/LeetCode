ddf1a5ca0fec9c0a7b1bcfd14711a5aab322450a
Ryan Liang <ryanliang88@outlook.com>
2019-08-03 18:32:03 -0700

    Added LengthOfLongestStringTest

---
+++ LeetCodeTests/LengthOfLongestStringTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using LeetCodeSolutions;
+
+namespace LeetCodeTests
+{
+    public class LengthOfLongestStringTest
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void Original_Question(string s, int expected)
+        {
+            // Arrange
+            var lengthOfLongestString = new LengthOfLongestString();
+
+            // Act
+            var actual = lengthOfLongestString.Solution(s);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /**
+        Thoughts:
+
+        pointer1 loops through the string
+        pointer2 starts from pointer1 and stops once I get a letter already in my character array
+
+        optimization:
+        pointer1 can jump to pointer2 once pointer2 stops
+        pointer1 can exit loop early once rest of string is shorter than maxSubstring
+
+        */
+    }
+}
