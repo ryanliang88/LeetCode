563588f506168fd05e5d46dcda8e97569b365205
Ryan Liang <ryanliang88@outlook.com>
2019-08-03 23:48:37 -0700

    Added LengthOfLongestSubstring, renamed files

---
+++ LeetCodeTests/LengthOfLongestSubstringTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using LeetCodeSolutions;
+
+namespace LeetCodeTests
+{
+    public class LengthOfLongestSubstringTest
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        public void Original_Questions(string s, int expected)
+        {
+            // Arrange
+            var lengthOfLongestString = new LengthOfLongestSubstring();
+
+            // Act
+            var actual = lengthOfLongestString.Solution(s);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
