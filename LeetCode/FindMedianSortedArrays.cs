d8d35fa6e38da49f879cb0f4440ec2d98bdf853a
Ryan Liang <ryanliang88@outlook.com>
2019-08-04 12:08:10 -0700

    Added FindMedianSortedArraysTest

---
+++ LeetCode/FindMedianSortedArrays.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions
+{
+    public class FindMedianSortedArrays
+    {
+        public double Solution(int[] nums1, int[] nums2)
+        {
+            return 0;
+        }
+    }
+}
